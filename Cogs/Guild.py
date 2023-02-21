4faf0143251d6803891ce195fd561a787cee191d
Woody Moon <woodkill@gmail.com>
2023-02-21 16:04:43 +0900

    길드등록, 길드확인 명령어 기능 완성

--- Cogs/Guild.py
+++ Cogs/Guild.py
@@ -4,11 +4,14 @@ from discord.ext import commands
 from discord import app_commands
 # from discord import Interaction
 # from discord import Object
+import btdb
 
 class Guild(commands.Cog):
 
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
+        import btdb
+        self.db :btdb.BtDb = bot.db
         self.logger = logging.getLogger('cog')
 
     @commands.Cog.listener()
@@ -26,10 +29,30 @@ class Guild(commands.Cog):
     #     await interaction.response.send_message('Answered')
 
     @commands.command(name=u"길드등록")
-    async def register_guild(self, ctx, *args):
-        self.logger.debug(f"{args}")
-        # self.logger.info(f"길드등록 command")
-        # await ctx.send("hi")
+    async def register_guild(self, ctx: commands.Context, *args):
+        self.logger.debug(f"길드등록 {args}")
+        if len(args) != 2:
+            await ctx.reply(f"사용법 : .길드등록 ***오딘서버명 길드명***")
+            return
+        odin_server_name = args[0]
+        odin_guild_name = args[1]
+        if not self.db.check_valid_server_name(odin_server_name):
+            await ctx.reply(f"올바른 오딘서버명이 아닙니다 : '{odin_server_name}'")
+            return
+        success = self.db.set_odin_guild_info(ctx.guild.id, odin_server_name, odin_guild_name)
+        if not success:
+            await ctx.reply(f"길드등록에 실패하였습니다.")
+            return
+        await ctx.reply(f"길드등록 완료 : {odin_server_name}/{odin_guild_name}")
+
+    @commands.command(name=u"길드확인")
+    async def check_guild(self, ctx: commands.Context):
+        self.logger.debug(f"길드확인")
+        success, odin_server_name, odin_guild_ame = self.db.get_odin_guild_info(ctx.guild.id)
+        if success:
+            await ctx.reply(f"{odin_server_name}/{odin_guild_ame}")
+        else:
+            await ctx.reply(f"등록된 길드가 없습니다.")
 
 
 async def setup(bot: commands.Bot) -> None:
--- btdb.py
+++ btdb.py
@@ -7,8 +7,9 @@ import logging
 """
  collection key
 """
-kROOT_COL_ODINDATA = "OdinData"
-kROOT_COL_ODINGUILD = u'OdinGuild'
+kCOL_ODINDATA = "OdinData"
+kCOL_ODINGUILD = u'OdinGuild'
+kCOL_BOSS = u'Boss'
 
 """
  document key
@@ -18,11 +19,9 @@ kDOC_ODINDATA = u'OdinData'
 """
  field name
 """
-kFIELD_SERVER_LIST = u'serverList'
-
-"""
- 오딘서버목록을 원격DB에서 로딩
-"""
+kFLD_SERVER_LIST = u'serverList'
+kFLD_SERVER_NAME = u'serverName'
+kFLD_GUILD_NAME = u'guildName'
 
 
 class BtDb():
@@ -35,13 +34,41 @@ class BtDb():
         # logger setting
         self.logger = logging.getLogger('db')
         # data storage
-        self.gServerSet = self.get_server_info()
+        self.serverSet = {}
+        # initialize
+        self.load_server_list()
 
-    def get_server_info(self):
-        doc = self.db.collection(kROOT_COL_ODINDATA).document(kDOC_ODINDATA).get()
-        serverList = doc.to_dict()[kFIELD_SERVER_LIST]
-        self.logger.info(f"오딘서버 로딩 완료 : {serverList}")
-        return set(serverList)
+    def load_server_list(self):
+        doc = self.db.collection(kCOL_ODINDATA).document(kDOC_ODINDATA).get()
+        server_list = doc.to_dict()[kFLD_SERVER_LIST]
+        self.logger.info(f"오딘서버목록 로딩 완료 : {server_list}")
+        self.serverSet = set(server_list)
+        return True
+
+    def check_valid_server_name(self, odin_server_name):
+        return odin_server_name in self.serverSet
+
+    def get_odin_guild_info(self, discord_guild_id):
+        str_discord_guild_id = str(discord_guild_id)
+        doc = self.db.collection(kCOL_ODINGUILD).document(str_discord_guild_id).get()
+        if not doc.exists:
+            self.logger.info(f"디스코드서버ID:{discord_guild_id} 로 등록된 오딘길드가 없음.")
+            return False, None, None
+        dic = doc.to_dict()
+        odin_server_name = dic[kFLD_SERVER_NAME]
+        odin_guild_name = dic[kFLD_GUILD_NAME]
+        self.logger.info(f"get odin guild : 디스코드서버ID:{discord_guild_id} 오딘길드:{odin_server_name}/{odin_guild_name}")
+        return True, odin_server_name, odin_guild_name
+
+    def set_odin_guild_info(self, discord_guild_id, odin_server_name, odin_guild_name):
+        str_discord_guild_id = str(discord_guild_id)
+        col_ref = self.db.collection(kCOL_ODINGUILD).document(str_discord_guild_id).set({
+            kFLD_SERVER_NAME: odin_server_name,
+            kFLD_GUILD_NAME: odin_guild_name
+        }, merge=True)
+        return True
+
+
 
 
 
